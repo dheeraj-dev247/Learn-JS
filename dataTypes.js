53eee4e36e2c478388a883e1aea944f3650432b5
dheeraj-dev247 <dheeraj.dev247@gmail.com>
2025-08-16 19:21:24 +0530

    added objects file

--- dataTypes.js
+++ dataTypes.js
@@ -7,4 +7,10 @@ let productName = "iphone 14"
 // boolean
 let isAvaliable = true;
 
+let y = null;
+
+let veryBigNumber = 878876786882943857987459872934579347898759874398579837459873498579348579834759347598438759347593487n
+
+console.log(veryBigNumber)
+
  
---
+++ objects.js
@@ -0,0 +1,15 @@
+let product = {
+    name: "Apple Iphone 11 pro max",
+    price: 150000,
+    discount: 15,
+    isAvaliable: false
+}
+
+let socialHandles = {
+    fb:9000,
+    insta:6700,
+    "linkedin":2000,
+    x:5000
+}
+
+console.log(socialHandles);

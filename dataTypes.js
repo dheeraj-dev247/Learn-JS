70ebffdd0b7da770f129c8ed9430f1c282dd41f5
dheeraj-dev247 <dheeraj.dev247@gmail.com>
2025-08-16 13:51:02 +0530

    initial commit of project files

---
+++ dataTypes.js
@@ -0,0 +1,10 @@
+let productPrice = 150000;
+let discount = 15;
+
+// string -- to store random text 
+let productName = "iphone 14" 
+
+// boolean
+let isAvaliable = true;
+
+ 
---
+++ variablesIntroductions.js
@@ -0,0 +1,19 @@
+let a = 5;
+
+var b = 30;
+
+const c = 18;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// variable names rules
+
+// alphabet, digit, underscore, dollar
+
+// digit not comes in starting
+
+// variable names are case sensitive
+
+// you cannot use a keyword for a variable naming -- eg let for if else var space nhi aa sakta

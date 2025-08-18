21b5e5055b253636eb012f3276b8841c17abf747
dheeraj-dev247 <dheeraj.dev247@gmail.com>
2025-08-18 23:04:25 +0530

    added loops

---
+++ loops/for.js
@@ -0,0 +1,21 @@
+// for(let i = 1; i<= 5; i++){
+// console.log(i)
+// }
+
+let array = [3,4,5,3,2]
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    console.log(element);
+}
+
+
+let a = 1;
+
+console.log(a++)
+console.log(a)
+
+let b = 1;
+
+console.log(++b)
+console.log(b)
